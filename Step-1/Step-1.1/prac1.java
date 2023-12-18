0bc74762d71fc1a4fbbc7be21ef2945d654fe567
Vipul Agnihotri <86738105+vibgyorvip@users.noreply.github.com>
2023-12-18 18:56:31 +0530

    Create prac1.java
    Basic Input and Output in Java Using Scanner Class
    3
    The entered number is :3

---
+++ Step-1/Step-1.1/prac1.java
@@ -0,0 +1,13 @@
+//Basic Input and Output in Java
+import java.util.Scanner;
+
+public class Prac1 {
+    public static void main(String[] args)
+    {
+        //taking a number as input
+        int number;
+        Scanner sc= new Scanner(System.in);
+        number= sc.nextInt();
+        System.out.println("The entered number is : "+number);
+    }
+}

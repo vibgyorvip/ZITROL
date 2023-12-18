b0eae81f9116fc76b2ba17ebae821e195763a5f3
Vipul Agnihotri <86738105+vibgyorvip@users.noreply.github.com>
2023-12-18 19:00:24 +0530

    Create prac2.java
    3
    4
    The value of x : 3 and the value of Y :4

---
+++ Step-1/Step-1.1/prac2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Prac1 {
+    public static void main(String[] args)
+    {
+        //taking a number as input
+        int x,y;
+        Scanner sc= new Scanner(System.in);
+        x= sc.nextInt();
+        y= sc.nextInt();
+        System.out.println("The value of x : "+x+" and the value of Y :"+y);
+    }
+}

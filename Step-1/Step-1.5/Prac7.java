9387ec6254e4e0f7fac2e6982d5dbcb446944fbb
Vipul Agnihotri <86738105+vibgyorvip@users.noreply.github.com>
2024-01-03 20:02:12 +0530

    Find Fibonacci Number

---
+++ Step-1/Step-1.5/Prac7.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Recursion {
+    static int findFebonacci(int n)
+    {
+        if(n == 0 || n==1) return n;
+
+        return findFebonacci(n-1)+findFebonacci(n-2);
+    }
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+
+
+        System.out.println(findFebonacci(n));
+    }
+}
